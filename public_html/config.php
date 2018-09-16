244419761d0866acb0c713fce0394214cefe3a4a
dragonairex <dragonairex@gmail.com>
2018-09-16 17:36:15 +0800

    init git

--- public_html/config.php
+++ public_html/config.php
@@ -1,14 +1,4 @@
 <?php
-// 000webhost
-// website name: unxemxanmrz
-// user: theredheadphone@gmail.com
-// pass: w/o
-
-
-// Name servers
-// ns01.000webhost.com
-// ns02.000webhost.com
-
 // Current version: PHP 7.1
 // http
 define('ROOT_HEADER_PATH','https://unxemxanmrz.000webhostapp.com');
@@ -17,12 +7,7 @@ define('FTP_HOST','');
 define('FTP_PORT','');
 define('FTP_USER','');
 define('FTP_PASS','');
-// Home directory	/storage/ssd5/447/6130447
-// Folder to upload files	public_html
-
 // db
-// InnoDB on MariaDB 10.1
-// phpmyadmin: https://databases.000webhost.com/
 define('DB_HOST','');
 define('DB_NAME','');
 define('DB_USER','');
